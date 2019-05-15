fb7d9e156a7335596e312ae3e9d609b12ee83b38
Brady Niemitalo Woods <49171897+BDPNC101@users.noreply.github.com>
2019-05-15 13:14:29 -0600

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,7 +1,18 @@
-# Car class
-    # constructor function
+class Car:
+    #Car class for representing an automobile.
 
-    #fill_up function
+    def __init__(self, initial_fuel):
+        #Create a new car with fuel
+        self.fuel = initial_fuel
+    
+    def add_gas(self, gas_added):
+        #Add fuel to the car
+        self.fuel += gas_added
 
-    #add_gas function
-
+    def fill_up(self):
+        #Fill up the car's fuel tank
+        if self.fuel >= 13.0:
+            return 0
+        amount = 13.0 - self.fuel
+        self.add_gas(amount)
+        return amount
