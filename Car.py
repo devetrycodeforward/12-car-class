600c8229d5570252b724237e2ee1aeef5ffa73cd
Nearlybird <49175582+Nearlybird@users.noreply.github.com>
2019-05-16 09:59:00 -0600

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,14 +1,12 @@
-# Car class
-Class Car:
+
+class Car:
     # constructor function
     def __init__(self, gas_level):
         self.gas = float(gas_level)
-        return self.gas
+
     #fill_up function
-    def fill_up(gas_up)
-        if self.gas + float(gas_up) >= 13:
+    def fill_up(self):
+        if self.gas >= 13:
             return 0
         else:
             return 13 - self.gas
-    #add_gas function
-
