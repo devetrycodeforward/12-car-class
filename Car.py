0d8befd683f80b13729d0074ac15fd9535718bc8
Fiona-Gorrie <47618889+Fiona-Gorrie@users.noreply.github.com>
2019-05-16 16:19:36 -0600

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,7 +1,16 @@
-# Car class
-    # constructor function
+class Car:
 
-    #fill_up function
+    def __init__(self, gas_level):
+        self.gas_level = float(gas_level) 
 
-    #add_gas function
+    def add_gas(self, gas_needed):
+        self.gas_level += float(gas_needed)
+
+    def fill_up(self):
+        gas_needed = 13.0 - self.gas_level
+        if gas_needed > 0:
+            self.add_gas(gas_needed)
+            return float(gas_needed)
+        else:
+            return 0
 
