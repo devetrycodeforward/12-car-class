733bb7a2e12db6dd5735e210fd9dd14e58012d30
ajpendergrass <ajpendergrass@gmail.com>
2019-05-15 20:43:04 -0600

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,7 +1,16 @@
-# Car class
-    # constructor function
+class Car:
+  def __init__(self, init_gas_level):
+    self.gas_level = init_gas_level
 
-    #fill_up function
+  def add_gas(self, gas_to_add):
+    self.add_gas = float(gas_to_add)
+    self.gas_level = (self.gas_level + gas_to_add)
+    return self.gas_level
+  
+  def fill_up(self):
+    if self.gas_level <13:
+      return (13 - (self.gas_level))
+    else:
+      return 0
 
-    #add_gas function
 
