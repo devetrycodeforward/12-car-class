2ab424bb8e2b26fc6fe6a610d0ef66c203c87765
eyetpn <eyetpn@gmail.com>
2019-05-16 19:22:13 -0600

    Update Car.py

--- Car.py
+++ Car.py
@@ -5,16 +5,13 @@ class Car:
     def get_gaslevel(self):
         return self.gas_level
 
-    def add_gas(x):
+    def add_gas(self, x):
         new_gaslevel = x + gas_level
         return new_gaslevel
     
-    def fill_up(y):
+    def fill_up(self, y):
         if y < 13:
             new_y = 13- y
             return new_y
         else: 
             return 0
-
-x = Car(14.0)
-print (x.fill_up())
